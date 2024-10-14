bbc2082067f31f3d1d03cc2739f63ceef9fc2b18
TheLegendaryDragon2004 <dragonxtremelx2004@gmail.com>
2024-10-14 23:01:14 +0530

    new

--- mern-project-main/backend/routes/reservationRoute.js
+++
@@ -1,8 +0,0 @@
-// reservation.js
-import express from 'express';
-import { sendReservation } from "../controller/reservation.js"; // Ensure the controller is correctly imported
-const router = express.Router();
-
-router.post("/send", sendReservation); // Route: /api/reservation/send (with prefix /api)
-
-export default router;
